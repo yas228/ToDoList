09596445edd2831aaf3dfdf831c7a00dfcea14fd
Yawo Siatitse <yawo.siatitse@gmail.com>
2018-12-16 02:58:16 -0500

    intial

---
+++ assets/js/todos.js
@@ -0,0 +1,21 @@
+//check off a task
+
+$("ul").on("click", "li", function () {
+	$(this).toggleClass("completed");
+});
+
+//delete a task
+$("ul").on("click", "span",function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13){
+		var newTodo = $(this).val();
+		$("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> "+newTodo+"</li>");
+
+	}
+});
